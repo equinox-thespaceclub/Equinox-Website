17d022192031b5aecd73fe7b13921d816b926681
Dipak <dipakchaudhari171@gmail.com>
2025-03-13 21:20:16 +0530

    Added my code

--- app/team/page.tsx
+++ app/team/page.tsx
@@ -1,4 +1,4 @@
-import { teamData } from '@/data/team';
+import { teamData } from '../data/team';
 import { FaInstagram, FaLinkedin, FaTwitter, FaGithub } from 'react-icons/fa';
 import Image from 'next/image';
 
